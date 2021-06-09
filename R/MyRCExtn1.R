86f6af0232b1b4b10bf3e05600378c58d82dd11e
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2021-06-09 13:07:06 +0200

    new testing function created for debungging: foo

--- R/MyRCExtn1.R
+++ R/MyRCExtn1.R
@@ -10,10 +10,15 @@
 
 Flugbahn <- function(v0,t,angle_Schussebenen,Ziel_Schussebenen,m,k)
 {
-  .Call( C_FlugbahnV3, as.numeric(v0), as.numeric(t), as.numeric(angle_Schussebenen),as.numeric(Ziel_Schussebenen), as.numeric(m),as.numeric(k))
+  .Call( C_Flugbahn, as.numeric(v0), as.numeric(t), as.numeric(angle_Schussebenen),as.numeric(Ziel_Schussebenen), as.numeric(m),as.numeric(k))
 }
 
 Multiply <- function(x,y)
 {
   .C( C_Multiply, x, y, numeric(1) ) [[3]]
+}
+
+foo <- function (x)
+{
+  .Call(C_foo, as.numeric(x), length(x))
 }
--- src/MyRCExtn1.c
+++ src/MyRCExtn1.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-//#include <vector>
-
 #include <R.h>
 #include <Rinternals.h>
+#include <Rmath.h>
+#include <stddef.h>
 #include <R_ext/Rdynload.h>
-
 
 /////////// .C interface style ////////////////
 // The functions should return void in order to deal with .C,
@@ -21,7 +17,7 @@ static const R_CMethodDef cMethods[] =
 {
 	// C functions extended by using .C interface
    {"Multiply",   (DL_FUNC) &Multiply, 3, argMultiply},
-   {"Divide",     (DL_FUNC) &Divide,   6, argDivide},
+   {"Divide",     (DL_FUNC) &Divide,   3, argDivide},
    {NULL, NULL, 0, NULL}
 };
 
@@ -30,13 +26,15 @@ static const R_CMethodDef cMethods[] =
 SEXP Flugbahn (SEXP v0, SEXP t, SEXP angle_Schussebenen,SEXP Ziel_Schussebenen,SEXP m, SEXP k);
 SEXP FlugbahnV2 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebenen,SEXP m, SEXP k);
 SEXP FlugbahnV3 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebenen,SEXP m, SEXP k);
+SEXP foo(SEXP x, SEXP y);
 
 R_CallMethodDef callMethods[] =
 {
 	// C functions extended by using .Call interface
 	{ "Flugbahn",   (DL_FUNC)&Flugbahn,  6 },
 	{ "FlugbahnV2",   (DL_FUNC)&FlugbahnV2,  6 },
-	{ "FlugbahnV3",   (DL_FUNC)&FlugbahnV2,  6 },
+	{ "FlugbahnV3",   (DL_FUNC)&FlugbahnV3,  6 },
+	{ "foo",   (DL_FUNC)&foo,  2 },
 	{ NULL, NULL, 0 }
 };
 
@@ -72,11 +70,11 @@ const static double pi = 3.14159265359; //Kreiszahl
 const static double g = -9.81;        // [m / s ^ 2]
 
 double long betrag (double long x, double long y){
-  return sqrt(pow(x,2)+pow(y,2));
+  return sqrt(R_pow(x,2)+R_pow(y,2));
 }
 
 double long a_t (double long t,double long v0[], double m, double k){
-  return (-1)*pow(betrag(v0[0],v0[1]),2)*k/m;
+  return (-1)*R_pow(betrag(v0[0],v0[1]),2)*k/m;
   }
 
 //////// Function to be used with .Call interface ////////////
@@ -88,7 +86,7 @@ SEXP Flugbahn (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebenen,
   //iterationen
   const static unsigned int  iter = 10000; // it is not possible to protect more memory than for 10000 iterations
   //Beschleunigung
-  double long a = (-1)*(pow(asReal(v0),2)*asReal(k))/asReal(m);
+  double long a = (-1)*(R_pow(asReal(v0),2)*asReal(k))/asReal(m);
 
   //Geschwindigkeitsänderung
   double long dv_x =  a*cos(angle)*asReal(t);
@@ -120,7 +118,7 @@ SEXP Flugbahn (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebenen,
     // letzte Geschwindigkeit
     double long v0_   = betrag(m_data[i-1][0],m_data[i-1][1]);
     // Luftwiderstand (Beschleunigung entgegen der Flugrichtung)
-    double long a    = -(1)*(pow(v0_,2)*asReal(k)) / asReal(m);
+    double long a    = -(1)*(R_pow(v0_,2)*asReal(k)) / asReal(m);
     //Geschwindigkeitsänderung
     double long dv_x =  a*cos(angle_)    * asReal(t);
     double long dv_y = (a*sin(angle_)+g) * asReal(t);   // -Erdbeschleunigung -9.81
@@ -164,7 +162,7 @@ SEXP FlugbahnV2 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
 {
   // Sequenz der Werte die abgespeichert werden müssen
   const unsigned int column = 5;
-  const unsigned int c_nb_values = 300;
+  const unsigned int c_nb_values = 50;
 
   //Datenspeicher von R lesbar
   SEXP result = PROTECT(allocVector(REALSXP, c_nb_values*column));
@@ -180,10 +178,10 @@ SEXP FlugbahnV2 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
   double* p_vy = (double*)calloc(iter, sizeof(double));
   double* p_sx = (double*)calloc(iter, sizeof(double));
   double* p_sy = (double*)calloc(iter, sizeof(double));
-  double* p_t = (double*)calloc(iter, sizeof(double));
+  double* p_t  = (double*)calloc(iter, sizeof(double));
 
   //Beschleunigung
-  long double  a = (-1) * (pow(asReal(v0), 2) * asReal(k)) / asReal(m);
+  long double  a = (-1) * (R_pow(asReal(v0), 2) * asReal(k)) / asReal(m);
 
   //Geschwindigkeitsänderung
   long double  dv_x = a * cos(asReal(angle_Schussebenen)) * asReal(t);
@@ -210,7 +208,7 @@ SEXP FlugbahnV2 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
     // letzte Geschwindigkeit
     double long v0_ = betrag(p_vx[i - 1], p_vy[i - 1]);
     // Luftwiderstand (Beschleunigung entgegen der Flugrichtung)
-    double long a = -(1) * (pow(v0_, 2) * asReal(k)) / asReal(m);
+    double long a = -(1) * (R_pow(v0_, 2) * asReal(k)) / asReal(m);
     //Geschwindigkeitsänderung
     double long dv_x = a * cos(angle_) * asReal(t);
     double long dv_y = (a * sin(angle_) - 9.81) * asReal(t);   // -Erdbeschleunigung -9.81
@@ -228,7 +226,7 @@ SEXP FlugbahnV2 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
     double  test = round(angle_ / pi * 180 * 1000) / 1000;
 
     //Abbruch: ist die alte Distanz kleiner als die neue oder der Winkel fast -90Grad
-    if (dist_ < betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i], REAL(Ziel_Schussebenen)[1] - p_sy[i]) ) {
+    if (dist_ < betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i], REAL(Ziel_Schussebenen)[1] - p_sy[i]) || test < -89.99) {
 
       int ratio = round(i / c_nb_values);
       int c_seq[c_nb_values + 1];
@@ -267,11 +265,12 @@ SEXP FlugbahnV2 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
   return result;
 }
 
+
 SEXP FlugbahnV3 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebenen,SEXP m, SEXP k)
 {
-  // Sequenz der Werte die abgespeichert werden müssen
+  // Werte die abgespeichert werden und an R zurueckgegeben werden
   const unsigned int column = 5;
-  const unsigned int c_nb_values = 300;
+  const unsigned int c_nb_values = 50;
 
   //Datenspeicher von R lesbar
   SEXP result = PROTECT(allocVector(REALSXP, c_nb_values*column));
@@ -280,48 +279,49 @@ SEXP FlugbahnV3 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
   double dist = betrag(REAL(Ziel_Schussebenen)[0], REAL(Ziel_Schussebenen)[1]);
 
   // init memory to save the results according to distance in respect of time
-  unsigned long int  iter = 2 * floor(dist / (asReal(v0) * asReal(t)));
+  unsigned long int  iter = 2 * round(dist / (asReal(v0) * asReal(t)));
 
-  //Pointers to memory
-
-  double* p_vx = (double *)R_alloc(iter, sizeof(double));
-  double*p_vy = (double *)R_alloc(iter, sizeof(double));
-  double* p_sx = (double*)R_alloc(iter, sizeof(double));
-  double* p_sy = (double*)R_alloc(iter, sizeof(double));
-  double* p_t = (double*)R_alloc(iter, sizeof(double));
+  // Pointers to user controlled memory in R
+  // https://colinfay.me/writing-r-extensions/the-r-api-entry-points-for-c-code.html
+  // 6.1.2 User-controlled memory
+  double* p_vx = (double *)Calloc(iter, double);
+  double* p_vy = (double *)Calloc(iter, double);
+  double* p_sx = (double *)Calloc(iter, double);
+  double* p_sy = (double *)Calloc(iter, double);
+  double* p_t  = (double *)Calloc(iter, double);
 
   //Beschleunigung
-  long double  a = (-1) * (pow(asReal(v0), 2) * asReal(k)) / asReal(m);
+  double  a = (-1) * (R_pow(asReal(v0), 2) * asReal(k)) / asReal(m);
 
   //Geschwindigkeitsänderung
-  long double  dv_x = a * cos(asReal(angle_Schussebenen)) * asReal(t);
-  long double  dv_y = (a * sin(asReal(angle_Schussebenen)) + g) * asReal(t); // -Erdbeschleunigung -9.81
+  double  dv_x = a * cos(asReal(angle_Schussebenen)) * asReal(t);
+  double  dv_y = (a * sin(asReal(angle_Schussebenen)) + g) * asReal(t); // -Erdbeschleunigung -9.81
 
   //Flugbahn inital
   p_vx[0] = asReal(v0)*cos(asReal(angle_Schussebenen)); // Anfangsgeschwindigkeit v_x
   p_vy[0] = asReal(v0)*sin(asReal(angle_Schussebenen)); // Anfangsgeschwindigkeit v_y
   p_vx[1] = p_vx[0] + dv_x;   // Init Geschwindigkeit v_x + Geschwindigkeitsänderung
   p_vy[1] = p_vy[0] + dv_y;   // Init Geschwindigkeit v_x + Geschwindigkeitsänderung
-  p_sx[0] = 0.0;  // init Position s_x
-  p_sy[0] = 0.0;  // init Position s_y
+  p_sx[0] = 0;  // init Position s_x
+  p_sy[0] = 0;  // init Position s_y
   p_sx[1] = p_vx[0] * asReal(t); // Position s_x
   p_sy[1] = p_vy[0] * asReal(t); // Position s_y
-  p_t[0] = 0.0; // t = 0
+  p_t[0] = 0; // t = 0
   p_t[1] = asReal(t);
 
   // Flugbahn
   for (unsigned int long i = 2; i < iter; i++) {
     // Flugwinkel
-    double long angle_ = atan((p_sy[i - 2] - p_sy[i - 1]) / (p_sx[i - 2] - p_sx[i - 1]));
+    double angle_ = atan((p_sy[i - 2] - p_sy[i - 1]) / (p_sx[i - 2] - p_sx[i - 1]));
     // letzte Distanz
-    double long dist_ = betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i - 1], REAL(Ziel_Schussebenen)[1] - p_sy[i - 1]);
+    double dist_ = betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i - 1], REAL(Ziel_Schussebenen)[1] - p_sy[i - 1]);
     // letzte Geschwindigkeit
-    double long v0_ = betrag(p_vx[i - 1], p_vy[i - 1]);
+    double v0_ = betrag(p_vx[i - 1], p_vy[i - 1]);
     // Luftwiderstand (Beschleunigung entgegen der Flugrichtung)
-    double long a = -(1) * (pow(v0_, 2) * asReal(k)) / asReal(m);
+    double a = -(1) * (R_pow(v0_, 2) * asReal(k)) / asReal(m);
     //Geschwindigkeitsänderung
-    double long dv_x = a * cos(angle_) * asReal(t);
-    double long dv_y = (a * sin(angle_) - 9.81) * asReal(t);   // -Erdbeschleunigung -9.81
+    double dv_x = a * cos(angle_) * asReal(t);
+    double dv_y = (a * sin(angle_) - 9.81) * asReal(t);   // -Erdbeschleunigung -9.81
 
     // Neue Geschwindigkeit
     p_vx[i] = p_vx[i - 1] + dv_x; // alte Geschwindigkeit x + delta v_x
@@ -336,7 +336,7 @@ SEXP FlugbahnV3 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
     double  test = round(angle_ / pi * 180 * 1000) / 1000;
 
     //Abbruch: ist die alte Distanz kleiner als die neue oder der Winkel fast -90Grad
-    if (dist_ < betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i], REAL(Ziel_Schussebenen)[1] - p_sy[i]) ) {
+    if (dist_ < betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i], REAL(Ziel_Schussebenen)[1] - p_sy[i]) || test < -89.99) {
 
       int ratio = round(i / c_nb_values);
       int c_seq[c_nb_values + 1];
@@ -365,12 +365,41 @@ SEXP FlugbahnV3 (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebene
     }
   }
 
-  free(p_vx);
-  free(p_vy);
-  free(p_sx);
-  free(p_sy);
-  free(p_t);
+  Free(p_vx);
+  Free(p_vy);
+  Free(p_sx);
+  Free(p_sy);
+  Free(p_t);
 
   UNPROTECT(1);
   return result;
 }
+
+SEXP foo(SEXP x, SEXP lenght)
+{
+  // User-controlled memory
+  // https://colinfay.me/writing-r-extensions/the-r-api-entry-points-for-c-code.html
+
+  // 6.1.1 Transient storage allocation
+  //x = (int *) R_alloc(10, sizeof(int));
+
+  // 6.1.2 User-controlled memory
+  double* p_x = (double *)Calloc(asReal(lenght), double);
+
+  //
+  for(unsigned long int i = 0; i < asReal(lenght); i++){
+    p_x[i] = REAL(x)[i]*2.233333;
+  }
+
+  //Datenspeicher von R lesbar
+  SEXP result = PROTECT(allocVector(REALSXP,asReal(lenght)+10));
+  //apply data to vector
+  for(unsigned long int i = 0; i < asReal(lenght); i++){
+    REAL(result)[i] = p_x[i];
+  }
+
+  Free(p_x);
+  UNPROTECT(1);
+  return result;
+}
+
